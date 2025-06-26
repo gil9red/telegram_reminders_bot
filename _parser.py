04ec940c4519b42b00df4f4a344da5f97d12cc2d
gil9red <ilya.petrash@inbox.ru>
2025-06-26 21:47:53 +0500

    Рефакторинг

--- _parser.py
+++ _parser.py
@@ -12,7 +12,10 @@ from datetime import datetime
 
 
 PATTERN_TARGET_DATETIME = re.compile(
-    r'(?:День рождения|Праздник|Напомни о) "(?P<target>\w+)" в (?P<day>\d{1,2}) (?P<month>\w+)(:? (?P<year>\d{4}))?',
+    r"(?:День рождения|Праздник|Напомни о) "
+    r'"(?P<target>\w+)" в (?P<day>\d{1,2}) (?P<month>\w+)'
+    r"(:?.*?(?P<year>\d{4}))?"
+    r"(:?.*?(?P<time>\d{2}:\d{2}))?",
     flags=re.IGNORECASE,
 )
 PATTERN_REPEAT = re.compile(
@@ -40,21 +43,21 @@ class TimeUnit:
     unit: TimeUnitEnum
 
 
-def get_target_datetime(text: str) -> datetime | None:
-    text = text.lower()
-
-    m = PATTERN_TARGET_DATETIME.search(text)
-    if not m:
-        return
-
-    # TODO:
-    return m
+@dataclass
+class ParseResult:
+    target: str
+    target_datetime: datetime
+    repeat: TimeUnit | None = None
 
 
-def get_repeat(text: str) -> TimeUnit | None:
-    text = text.lower()
+@dataclass
+class Defaults:
+    hours: int
+    minutes: int
 
-    m = PATTERN_REPEAT.search(text)
+
+def get_repeat(command: str) -> TimeUnit | None:
+    m = PATTERN_REPEAT.search(command)
     if not m:
         return
 
@@ -73,6 +76,63 @@ def get_repeat(text: str) -> TimeUnit | None:
             return
 
 
+def parse_month(month_value: str) -> int:
+    return [
+        "января",
+        "февраля",
+        "марта",
+        "апреля",
+        "мая",
+        "июня",
+        "июля",
+        "августа",
+        "сентября",
+        "октября",
+        "ноября",
+        "декабря",
+    ].index(month_value) + 1
+
+
+def parse_command(
+    command: str,
+    default: Defaults = Defaults(hours=11, minutes=0),
+) -> ParseResult | None:
+    command = command.lower()
+
+    m = PATTERN_TARGET_DATETIME.search(command)
+    if not m:
+        return
+
+    print(m.groupdict())
+    day: int = int(m.group("day"))
+    month: int = parse_month(m.group("month"))
+
+    year_value: str | None = m.group("year")
+    if year_value is not None:
+        year: int = int(year_value)
+    else:
+        year: int = datetime.now().year
+
+    time_value: str | None = m.group("time")
+    if time_value:
+        parts = time_value.split(":")
+        hours, minutes = map(int, parts)
+    else:
+        hours = default.hours
+        minutes = default.minutes
+
+    # TODO: Проверить високосные даты
+    target_datetime = datetime(
+        day=day, month=month, year=year, hour=hours, minute=minutes
+    )
+
+    return ParseResult(
+        target=m.group("target"),
+        target_datetime=target_datetime,
+        repeat=get_repeat(command),
+    )
+
+
 # TODO:
 text = """
 День рождения "xxx" в 10 февраля. Повтор раз в год. Напоминать за месяц, за неделю, за 3 дня, за день, в тот же день
@@ -80,6 +140,9 @@ text = """
 День рождения "xxx" в 10 февраля. Повтор раз в месяц. Напоминать за месяц, за неделю, за 3 дня, за день, в тот же день
 День рождения "xxx" в 10 февраля. Повтор раз в неделю. Напоминать за месяц, за неделю, за 3 дня, за день, в тот же день
 День рождения "xxx" в 10 февраля. Повтор каждый день
+День рождения "xxx" в 10 февраля в 14:55. Повтор каждый день
+День рождения "xxx" в 10 февраля 2026 года. Повтор каждый день
+День рождения "xxx" в 10 февраля 2026 года в 14:55. Повтор каждый день
 День рождения "xxx" в 10 февраля. Напоминать за месяц, за неделю, за 3 дня, за день, в тот же день
 Праздник "xxx" в 10 февраля. Повтор каждый год. Напоминать в тот же день
 Праздник "xxx" в 10 февраля 2026 года. Повтор каждый год. Напоминать в тот же день
@@ -89,7 +152,5 @@ text = """
 
 for line in text.splitlines():
     print(line)
-    print(get_target_datetime(line))
-    repeat: TimeUnit | None = get_repeat(line)
-    print(repeat)
+    print(parse_command(line))
     print()
