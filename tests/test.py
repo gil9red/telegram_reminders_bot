5b74af775e23379e86dc08c5cc6f3c257818e9ea
gil9red <ilya.petrash@inbox.ru>
2025-07-15 13:30:30 +0500

    Обновлено. tests/test.py. Добавлены тесты для перечислений

--- tests/test.py
+++ tests/test.py
@@ -16,6 +16,7 @@ from db import (
     Reminder,
     db,
 )
+from parser import TimeUnitEnum, TimeUnitWeekDayEnum
 
 
 # NOTE: https://docs.peewee-orm.com/en/latest/peewee/database.html#testing-peewee-applications
@@ -33,6 +34,42 @@ class TestCaseDb(unittest.TestCase):
     def test_TODO(self):
         # TODO:
         pass
+
+
+# TODO:
+class TestCaseParser(unittest.TestCase):
+    def test_TimeUnitEnum_days(self):
+        self.assertEqual(365, TimeUnitEnum.YEAR.days())
+        self.assertEqual(30, TimeUnitEnum.MONTH.days())
+        self.assertEqual(7, TimeUnitEnum.WEEK.days())
+        self.assertEqual(1, TimeUnitEnum.DAY.days())
+
+        for value, enum_value in [
+            ("YEAR", TimeUnitEnum.YEAR),
+            ("MONTH", TimeUnitEnum.MONTH),
+            ("WEEK", TimeUnitEnum.WEEK),
+            ("DAY", TimeUnitEnum.DAY),
+        ]:
+            with self.subTest(value=value, enum_value=enum_value):
+                self.assertEqual(value, enum_value.value)
+                self.assertEqual(value, enum_value.name)
+                self.assertEqual(enum_value, TimeUnitEnum(value))
+                self.assertEqual(enum_value, TimeUnitEnum[enum_value.name])
+
+    def test_TimeUnitWeekDayEnum(self):
+        for value, enum_value in [
+            (1, TimeUnitWeekDayEnum.MONDAY),
+            (2, TimeUnitWeekDayEnum.TUESDAY),
+            (3, TimeUnitWeekDayEnum.WEDNESDAY),
+            (4, TimeUnitWeekDayEnum.THURSDAY),
+            (5, TimeUnitWeekDayEnum.FRIDAY),
+            (6, TimeUnitWeekDayEnum.SATURDAY),
+            (7, TimeUnitWeekDayEnum.SUNDAY),
+        ]:
+            with self.subTest(value=value, enum_value=enum_value):
+                self.assertEqual(value, enum_value.value)
+                self.assertEqual(enum_value, TimeUnitWeekDayEnum(value))
+                self.assertEqual(enum_value, TimeUnitWeekDayEnum[enum_value.name])
 
 
 if __name__ == "__main__":
