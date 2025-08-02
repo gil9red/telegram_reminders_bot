27ea5c3fe76b17de06813b16942e013dd2eb022c
gil9red <ilya.petrash@inbox.ru>
2025-08-03 01:54:49 +0500

    Рефакторинг. tests/. Разделение тестов парсера и БД на отдельные файлы

---
+++ tests/test_db.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = "ipetrash"
+
+
+import unittest
+from datetime import datetime, timedelta
+
+from peewee import SqliteDatabase
+
+from db import (
+    BaseModel,
+    # TODO:
+    Reminder,
+    db,
+)
+
+
+# NOTE: https://docs.peewee-orm.com/en/latest/peewee/database.html#testing-peewee-applications
+class TestCaseDb(unittest.TestCase):
+    def setUp(self):
+        self.models = BaseModel.get_inherited_models()
+        self.test_db = SqliteDatabase(":memory:")
+        self.test_db.bind(self.models, bind_refs=False, bind_backrefs=False)
+        self.test_db.connect()
+        self.test_db.create_tables(self.models)
+
+    def tearDown(self):
+        db.bind(self.models, bind_refs=False, bind_backrefs=False)
+
+    def test_TODO(self):
+        # TODO:
+        1 / 0
+
+
+if __name__ == "__main__":
+    unittest.main()
--- tests/test_parser.py
+++ tests/test_parser.py
@@ -7,14 +7,6 @@ __author__ = "ipetrash"
 import unittest
 from datetime import datetime, timedelta
 
-from peewee import SqliteDatabase
-
-from db import (
-    BaseModel,
-    # TODO:
-    Reminder,
-    db,
-)
 from parser import (
     TimeUnitEnum,
     TimeUnitWeekDayEnum,
@@ -26,27 +18,10 @@ from parser import (
     parse_month,
     get_repeat_every,
     parse_repeat_before,
+    parse_command,
 )
 
 
-# NOTE: https://docs.peewee-orm.com/en/latest/peewee/database.html#testing-peewee-applications
-class TestCaseDb(unittest.TestCase):
-    def setUp(self):
-        self.models = BaseModel.get_inherited_models()
-        self.test_db = SqliteDatabase(":memory:")
-        self.test_db.bind(self.models, bind_refs=False, bind_backrefs=False)
-        self.test_db.connect()
-        self.test_db.create_tables(self.models)
-
-    def tearDown(self):
-        db.bind(self.models, bind_refs=False, bind_backrefs=False)
-
-    def test_TODO(self):
-        # TODO:
-        1 / 0
-
-
-# TODO:
 class TestCaseParserCommon(unittest.TestCase):
     def test_TimeUnitEnum_days(self):
         self.assertEqual(365, TimeUnitEnum.YEAR.days())
@@ -223,6 +198,7 @@ class TestCaseParserCommon(unittest.TestCase):
                 self.assertEqual(number, parse_month(month))
 
     def test_parse_command(self):
+        # TODO:
         1 / 0
 
 
