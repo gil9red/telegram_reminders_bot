a5c0a502bdaab485ef10f8ec1df95f21ecc3aaa6
gil9red <ilya.petrash@inbox.ru>
2025-08-13 03:44:55 +0500

    Обновлено. tests/test_common.py. Добавлены тесты test_convert_tz и test_get_tz

--- tests/test_common.py
+++ tests/test_common.py
@@ -6,10 +6,17 @@ __author__ = "ipetrash"
 
 import re
 
-from datetime import datetime
+from datetime import datetime, tzinfo
 from unittest import TestCase
 
-from common import datetime_to_str, prepare_text, get_int_from_match
+from common import (
+    datetime_to_str,
+    prepare_text,
+    get_int_from_match,
+    get_tz,
+    convert_tz,
+    ZoneInfoNotFoundError,
+)
 
 
 class TestCaseCommon(TestCase):
@@ -49,3 +56,35 @@ class TestCaseCommon(TestCase):
                 re.search(r"(?P<number>\d+)", "abc"), name="number", default=1
             ),
         )
+
+    def test_convert_tz(self):
+        dt: datetime = datetime(year=2025, month=8, day=9, hour=18, minute=0)
+        tz_utc: tzinfo = get_tz("UTC")
+        tz_0230: tzinfo = get_tz("+02:30")
+
+        self.assertEqual(
+            datetime(year=2025, month=8, day=9, hour=15, minute=30),
+            convert_tz(
+                dt=dt,
+                from_tz=tz_0230,
+                to_tz=tz_utc,
+            ),
+        )
+
+        self.assertEqual(
+            datetime(year=2025, month=8, day=9, hour=20, minute=30),
+            convert_tz(
+                dt=dt,
+                from_tz=tz_utc,
+                to_tz=tz_0230,
+            ),
+        )
+
+    def test_get_tz(self):
+        self.assertEqual("UTC+02:30", str(get_tz("+02:30")))
+        self.assertEqual("UTC-02:30", str(get_tz("-02:30")))
+        self.assertEqual("UTC", str(get_tz("UTC")))
+        self.assertEqual("Europe/Moscow", str(get_tz("Europe/Moscow")))
+
+        with self.assertRaises(ZoneInfoNotFoundError):
+            get_tz("dfgsdfsdfdsf")
