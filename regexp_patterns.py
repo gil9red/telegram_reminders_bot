106a5399a7941984f702b5a76633b49e6ce4647f
gil9red <ilya.petrash@inbox.ru>
2025-08-24 18:18:00 +0500

    Рефакторинг. regexp_patterns.py

--- regexp_patterns.py
+++ regexp_patterns.py
@@ -13,6 +13,12 @@ PATTERN_REMINDER_PAGE = re.compile(r"^reminder page=(?P<page>\d+)$")
 # TODO:
 # PATTERN_DELETE_MESSAGE = "delete_message"
 
+# TODO:
+PATTERN_SHOW_ORIGINAL_MESSAGE = re.compile(r"^reminder#(?P<id>\d+)-orig-mess$")
+
+# TODO:
+PATTERN_DELETE = re.compile(r"^reminder#(?P<id>\d+)-delete$")
+
 COMMAND_START = "start"
 COMMAND_HELP = "help"
 
@@ -29,4 +35,16 @@ if __name__ == "__main__":
     assert (
         fill_string_pattern(PATTERN_REMINDER_PAGE, 999_999_999)
         == "reminder page=999999999"
+    )
+
+    print(fill_string_pattern(PATTERN_SHOW_ORIGINAL_MESSAGE, 999_999_999))
+    assert (
+        fill_string_pattern(PATTERN_SHOW_ORIGINAL_MESSAGE, 999_999_999)
+        == "reminder#999999999-orig-mess"
+    )
+
+    print(fill_string_pattern(PATTERN_DELETE, 999_999_999))
+    assert (
+            fill_string_pattern(PATTERN_DELETE, 999_999_999)
+            == "reminder#999999999-delete"
     )
